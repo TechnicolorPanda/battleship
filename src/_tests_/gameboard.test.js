1eb5c030e88484c0af35dd1782c195524499b7a0
LorriAnn <LorriAnn7@att.net>
2021-03-22 10:56:31 -0400

    registers hit or miss for incoming attack

--- src/_tests_/gameboard.test.js
+++ src/_tests_/gameboard.test.js
@@ -1,9 +1,17 @@
-import { createBoard, placeShip } from '../factories/gameboard.js';
+import { createBoard, placeShip, receiveAttack } from '../factories/gameboard.js';
 
 test('creates empty gameboard', () => {
   expect (createBoard()).toStrictEqual([false, false, false, false, false, false, false, false, false, false]);
 })
 
 test('places ship on coordinates', () => {
-  expect (placeShip('carrier', 'C', 4)).toContain(7);
+  expect (placeShip('carrier', 'C', 4)).toContainEqual([2, 7]);
+})
+
+test('attacks ship at coordinates', () => {
+  expect (receiveAttack([[2, 3], [2, 4], [2, 5]], 'C', 4)).toBeTruthy();
+})
+
+test('attack ship by coodinates', () => {
+  expect (receiveAttack([[1, 2], [2, 2], [3, 2], [4, 2]], 'G', 8)).toBeFalsy();
 })
--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -1,4 +1,12 @@
 import shipLength from './Ship.js';
+
+  const findX = (rowLetter) => {
+    return rowLetter.charCodeAt(0) - 65;
+  }
+
+  const findY = (columnNumber) => {
+    return columnNumber;
+  }
  
   export const createBoard = () => {
     let newBoard = Array(10).fill(0).map(row => new Array(10).fill(false));
@@ -8,9 +16,22 @@ import shipLength from './Ship.js';
   export const placeShip = (shipType, row, column) => {
     let shipCoordinates = [];
     for (let i = 0; i < shipLength(shipType); i++) {
-      shipCoordinates = shipCoordinates.concat([(row.charCodeAt(0) - 65), (column - 1 + i)]);
+      shipCoordinates = shipCoordinates.concat([
+        [findX(row),
+         findY(column) + i
+        ]
+      ]);
     }
     return shipCoordinates; 
+  }
+
+  export const receiveAttack = (shipCoordinates, row, column) => {
+    for (let i = 0; i < shipCoordinates.length; i++) {
+      if (JSON.stringify(shipCoordinates[i]) === JSON.stringify([findX(row), findY(column)])) {
+        return true;
+      }
+    }
+    return false;
   }
 
    
