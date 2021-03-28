9e9b869ae326fd3ba0d4a2d509344ffd40c1bdd9
LorriAnn <LorriAnn7@att.net>
2021-03-28 19:45:36 -0400

    resets tests to 3/23/21 to rework factories

--- src/_tests_/gameboard.test.js
+++ src/_tests_/gameboard.test.js
@@ -1,29 +1,28 @@
-import Gameboard from '../factories/Gameboard';
+import { createBoard, placeShip, receiveAttack, recordHit } from '../factories/Gameboard.js';
 
 // const board = Gameboard();
 
 test('creates empty gameboard', () => {
-  expect (Gameboard()).toStrictEqual([false, false, false, false, false, false, false, false, false, false]);
+  expect (createBoard(0)).toStrictEqual([false, false, false, false, false, false, false, false, false, false]);
 })
 
-// test('places ship on coordinates', () => {
-//   board.placeShip('carrier', 'C', 4);
-//   expect(board.shipCoordinates).toContainEqual([2, 7]);
-// })
+test('places ship on coordinates', () => {
+  expect(placeShip('carrier', 'C', 4)).toContainEqual([2, 7]);
+})
 
-// test('attacks ship at coordinates', () => {
-//   expect (receiveAttack([[2, 3], [2, 4], [2, 5]], 'C', 4)).toBeTruthy();
-// })
+test('attacks ship at coordinates', () => {
+  expect (receiveAttack([[2, 3], [2, 4], [2, 5]], 'C', 4)).toBeTruthy();
+})
 
-// test('attack ship by coodinates', () => {
-//   expect (receiveAttack([[1, 2], [2, 2], [3, 2], [4, 2]], 'G', 8)).toBeFalsy();
-// })
+test('attack ship by coodinates', () => {
+  expect (receiveAttack([[1, 2], [2, 2], [3, 2], [4, 2]], 'G', 8)).toBeFalsy();
+})
 
-// test('records hit location', () => {
-//   expect (recordHit('G', 8)).toEqual([false, false, false, false, false, false, false, true, false, false]);
-// })
+test('records hit location', () => {
+  expect (recordHit('G', 8)).toEqual([false, false, false, false, false, false, false, true, false, false]);
+})
 
-// test('records next hit location', () => {
-//   expect (recordHit('B', 2)).toStrictEqual([false, true, false, false, false, false, false, false, false, false])
-// })
+test('records next hit location', () => {
+  expect (recordHit('B', 2)).toStrictEqual([false, true, false, false, false, false, false, false, false, false])
+})
 
--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -1,11 +1,4 @@
-import React, { useState, useEffect } from 'react';
 import shipLength from './Ship.js';
-import '../styles/gameboard.css';
-import uniqid from 'uniqid';
-
-const Gameboard = () => {
-  const [shipCoordinates, setShipCoordinates] = useState('');
-  const [newBoard, setNewBoard] = useState([]);
 
   const findX = (rowLetter) => {
     return rowLetter.charCodeAt(0) - 65;
@@ -15,19 +8,24 @@ const Gameboard = () => {
     return columnNumber;
   }
 
-  useEffect(() => {
-    setNewBoard(Array(10).fill(0).map(row => new Array(10).fill(false)));
-  },[])
-
-  const placeShip = (shipType, row, column) => {
-    for (let i = 0; i < shipLength(shipType); i++) {
-      setShipCoordinates(shipCoordinates => (shipCoordinates.concat([
-        [findX(row), findY(column) + i]
-      ])));
-    }
+  export const createBoard = (row) => {
+    let newBoard = Array(10).fill(0).map(row => new Array(10).fill(false));
+    return newBoard[row];
   }
 
-  const receiveAttack = (shipCoordinates, row, column) => {
+  export const placeShip = (shipType, row, column) => {
+    let shipCoordinates = [];
+    for (let i = 0; i < shipLength(shipType); i++) {
+      shipCoordinates = shipCoordinates.concat([
+        [findX(row),
+         findY(column) + i
+        ]
+      ]);
+    }
+    return shipCoordinates; 
+  }
+
+  export const receiveAttack = (shipCoordinates, row, column) => {
     for (let i = 0; i < shipCoordinates.length; i++) {
       if (JSON.stringify(shipCoordinates[i]) === JSON.stringify([findX(row), findY(column)])) {
         return true;
@@ -36,110 +34,9 @@ const Gameboard = () => {
     return false;
   }
 
-  const createNewRow = (row) => {
-    return (Array(10).fill(0).map(row => new Array(10).fill(false)));
-  }
-
-  const recordHit = (row, column) => {
+  export const recordHit = (row, column) => {
     let columnNumber = findY(column) - 1;
-    let newRow = createNewRow(findX(row));
-    newRow.splice(columnNumber, 1, true);
-    console.log(newRow);
+    let newBoard = createBoard(findX(row));
+    newBoard.splice(columnNumber, 1, true);
+    return newBoard;
   }
-
-  const createGameboard = (shipType, row, column) => {
-    placeShip(shipType, row, column);
-    receiveAttack(shipCoordinates, row, column);
-    recordHit(row, column);
-  }
-
-  const initiateAttack = (event, index) => {
-    event.target.className = 'background-red'; 
-    let coordinates = 'B2';
-    // let coordinates = event.target.value;
-    let row = coordinates.charAt(0);
-    let column = coordinates.charAt(1);
-    recordHit(row, column);
-  }
-
-  return (
-    <div className={`game-board`}>
-      <div>Battleship</div>
-      <div>
-        <table className = 'center'>
-        {newBoard.slice(1, newBoard.length).map((column, index) => {
-            return (
-              <tbody key = {uniqid()}>
-              <tr>
-                <td 
-                  key = {uniqid()}
-                  className = {column[0] ? 'background-red': 'background-blue'}
-                  value = {'A' + index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = {column[1] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = {column[2] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = {column[3] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = {column[4] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td                   
-                  key = {uniqid()}
-                  className = {column[5] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = {column[6] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = 
-                  {column[7] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = {column[8] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-                <td 
-                  key = {uniqid()}
-                  className = {column[9] ? 'background-red': 'background-blue'}
-                  value = {index}
-                  onClick = {initiateAttack}
-                ></td>
-              </tr>
-              </tbody>
-            );
-          })}
-        </table>
-      </div>
-    </div>
-  )
-}
-
-export default Gameboard;
