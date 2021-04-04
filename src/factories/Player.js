9126734813699426f78cd91ca90bb8b83e5e1fbe
LorriAnn <LorriAnn7@att.net>
2021-04-03 21:24:57 -0400

    adds random computer play"

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -2,6 +2,20 @@ import Gameboard from './Gameboard.js';
 
 const Player = () => {
 
+    const randomRow = () => {
+        let rowCoordinate = Math.floor(Math.random() * 10) + 65;
+        return String.fromCharCode(rowCoordinate);
+      }
+  
+      const randomColumn = () => {
+        return Math.floor(Math.random() * 10);
+      }
+  
+      const randomAlignment = () => {
+        let randomNumber = Math.floor(Math.random() * 2);
+        return (randomNumber === 0) ? 'horizontal': 'vertical';
+      }
+
     const placeCarrier = (row, column, alignment) => {
       return Gameboard().shipPlacement('carrier', row, column, alignment);
     }
@@ -22,26 +36,21 @@ const Player = () => {
       return Gameboard().shipPlacement('patrol boat', row, column, alignment);
     }
 
-    const randomRow = () => {
-      let rowCoordinate = Math.floor(Math.random() * 10) + 65;
-      return String.fromCharCode(rowCoordinate);
-    }
-
-    const randomColumn = () => {
-      return Math.floor(Math.random() * 10);
-    }
-
-    const randomAlignment = () => {
-      let randomNumber = Math.floor(Math.random() * 2);
-      return (randomNumber === 0) ? 'horizontal': 'vertical';
+    const computerPlacesBoat = () => {
+      placeCarrier(randomRow(), randomColumn(), randomAlignment());
     }
 
     const enterPlay = (row, column) => {
       Gameboard().recordHit(row, column);
       if (Gameboard().shipHit(row, column)) {
         Gameboard().deductHitPoints(row, column)
-      };
-      
+      }   
+    }
+
+    // TODO: ensure that computer play hits space marked false;
+
+    const computerPlay = () => {
+      enterPlay(randomRow(), randomColumn());
     }
 
     return { placeCarrier, enterPlay };
