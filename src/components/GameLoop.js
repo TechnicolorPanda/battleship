fb586d2efc7b8e8ba14a8a4c5dc5de2822d9f100
LorriAnn <LorriAnn7@att.net>
2021-04-09 10:07:07 -0400

    adds computer play board

--- src/components/GameLoop.js
+++ src/components/GameLoop.js
@@ -5,26 +5,31 @@ import '../styles/gameboard.css';
 
 const GameLoop = () => {
   const [newBoard, setNewBoard] = useState([]);
+  const [computerBoard, setComputerBoard] = useState([]);
 
   useEffect(() => {
     setNewBoard(Gameboard().createBoard());
+    setComputerBoard(Gameboard().createBoard());
   },[])
+
+  // save board to local storage
 
   const initiateAttack = (event) => {
     let coordinates = event.target.getAttribute('value');
-    let column = parseFloat(coordinates.charAt(1)) + parseFloat(1);  // 3
-    let row = coordinates.charAt(0); //A
-    setNewBoard(Gameboard().changeBoard(row, column));
+    let column = parseFloat(coordinates.charAt(1)) + parseFloat(1);
+    let row = coordinates.charAt(0);
+    setNewBoard (Gameboard().changeBoard(row, column));
   }
 
   return (
-        <div className={`game-board`}>
-          <div>Battleship</div>
-          <div>
-            <table className = 'center'>
+    <div className={`game-board`}>
+      <h1>Battleship</h1>
+        <div>
+          <table className = 'player'>
+          <h2>Player</h2>
             {newBoard.slice(1, newBoard.length).map((column, index) => {
-                return (
-                  <tbody key = {uniqid()}>
+              return (
+                <tbody key = {uniqid()}>
                   <tr>
                     <td 
                       key = {uniqid()}
@@ -87,12 +92,75 @@ const GameLoop = () => {
                       onClick = {initiateAttack}
                     ></td>
                   </tr>
-                  </tbody>
-                );
-              })}
-            </table>
-          </div>
-        </div>
+                </tbody>
+              );
+            })}
+          </table>
+      </div>
+      <div>
+          <table className = 'computer'>
+            <h2>Computer</h2>
+            {computerBoard.slice(1, computerBoard.length).map((column, index) => {
+              return (
+                <tbody key = {uniqid()}>
+                  <tr>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[0] ? 'background-red': 'background-blue'}
+                      value = {'0' + (index)}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[1] ? 'background-red': 'background-blue'}
+                      value = {'1' + index}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[2] ? 'background-red': 'background-blue'}
+                      value = {'2' + index}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[3] ? 'background-red': 'background-blue'}
+                      value = {'3' + index}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[4] ? 'background-red': 'background-blue'}
+                      value = {'4' + index}
+                    ></td>
+                    <td                   
+                      key = {uniqid()}
+                      className = {column[5] ? 'background-red': 'background-blue'}
+                      value = {'5' + index}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[6] ? 'background-red': 'background-blue'}
+                      value = {'6' + index}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[7] ? 'background-red': 'background-blue'}
+                      value = {'7' + index}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[8] ? 'background-red': 'background-blue'}
+                      value = {'8' + index}
+                    ></td>
+                    <td 
+                      key = {uniqid()}
+                      className = {column[9] ? 'background-red': 'background-blue'}
+                      value = {'9' + index}
+                    ></td>
+                  </tr>
+                </tbody>
+            );
+          })}
+        </table>
+      </div>
+    </div>
   )
 }
 
